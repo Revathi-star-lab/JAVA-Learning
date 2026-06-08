82df3731c74c62c3401266e107731625989cf8f1
Revathi Pilli <revathipilli421@gmail.com>
2026-06-08 11:25:34 +0530

    my java preparation

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld{
+    public static void main(String[]args){
+        // System.out.println("Hello");
+        //Myself
+        System.out.println("My name is Revathi");
+        System.out.println("I'm B.tech student");
+        System.out.println("I'm Studying in MITS");
+        System.out.println("I want to get a good job");
+        System.out.println("I'm a curious person fighting to learn new skills with my slep ");
+
+    }
+}
---
+++ RemoveDuplicates.java
@@ -0,0 +1,16 @@
+import java.util.LinkedHashSet;
+
+public class RemoveDuplicates {
+    public static void main(String[] args) {
+
+        int arr[] = {1,2,3,2,4,4,5,1};
+
+        LinkedHashSet<Integer> set = new LinkedHashSet<>();
+
+        for(int num : arr) {
+            set.add(num);
+        }
+
+        System.out.println(set);
+    }
+}
